9bc77d5713414ef35cc7569d8f263404b200c5fd
Rojen Arda Şeşen <sesen19@itu.edu.tr>
2024-09-07 02:15:09 +0300

    Include new routes for lectures and groups

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
 
-from app.api.routes import login, users, utils, posts, events
+from app.api.routes import groups, lectures, login, users, utils, posts, events
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
-api_router.include_router(posts.router, prefix="/posts", tags=["posts"])
+api_router.include_router(posts.router, prefix="/posts", tags=["posts", "courses"])
 api_router.include_router(events.router, prefix="/events", tags=["events"])
+api_router.include_router(lectures.router, prefix="/lectures", tags=["lectures"])
+api_router.include_router(groups.router, prefix="/groups", tags=["groups"])
